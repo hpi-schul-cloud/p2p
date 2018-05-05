155578425af372c5ef8c8f3bf84fce7580610fe2
Maximilian Kroschewski <jmakro@posteo.de>
2018-05-05 20:45:43 +0200

    dev backup

--- demo/public-data-channel/signal-server/js/client.js
+++ demo/public-data-channel/signal-server/js/client.js
@@ -8,119 +8,181 @@ const STUN_SERVER = {
   ]
 }
 
-let peerConn = new RTCPeerConnection(STUN_SERVER);
-let dataChannel = peerConn.createDataChannel('test');
+/****************************************************************************
+ * WebRTC peer connection and data channel
+ ****************************************************************************/
 
-peerConn.onicecandidate = function(event) {
-  console.log('icecandidate event:', event);
-  if (event.candidate) {
-    socket.emit('send_offer',{
-      type: 'candidate',
-      label: event.candidate.sdpMLineIndex,
-      id: event.candidate.sdpMid,
-      candidate: event.candidate.candidate,
-    });
+let peerConn;
+let dataChannel;
+
+function signalingMessageCallback(message) {
+  if (message.type === 'offer') {
+    console.log('Got offer. Sending answer to peer.');
+    peerConn.setRemoteDescription(message);
+    peerConn.createAnswer(onLocalSessionCreated);
+
+  } else if (message.type === 'answer') {
+    console.log('Got answer.');
+    peerConn.setRemoteDescription(message);
+
+  } else if (message.type === 'candidate') {
+    peerConn.addIceCandidate(message.candidate);
+
+  }
+}
+
+function createPeerConnection(isInitiator, config) {
+  console.log('Creating Peer connection as initiator?', isInitiator, 'config:',
+      config);
+  peerConn = new RTCPeerConnection(config);
+
+// send any ice candidates to the other peer
+  peerConn.onicecandidate = function(event) {
+    console.log('icecandidate event:', event);
+    if (event.candidate) {
+      sendMessage({
+        type: 'candidate',
+        label: event.candidate.sdpMLineIndex,
+        id: event.candidate.sdpMid,
+        candidate: event.candidate.candidate
+      });
+    } else {
+      console.log('End of candidates.');
+    }
+  };
+
+  if (isInitiator) {
+    console.log('Creating Data Channel');
+    dataChannel = peerConn.createDataChannel('test');
+    onDataChannelCreated(dataChannel);
+
+    console.log('Creating an offer');
+    peerConn.createOffer(onLocalSessionCreated);
   } else {
-    console.log('End of candidates.');
+    console.log('NILS');
+    peerConn.ondatachannel = function(event) {
+      console.log('ondatachannel:', event.channel);
+      dataChannel = event.channel;
+      onDataChannelCreated(dataChannel);
+    };
   }
-};
+}
 
-function createConnection(){
-  dataChannel.onopen = function () {
-    console.log("datachannel open");
-  };
-
-  peerConn.createOffer().then((sdp)=>{
-    console.log('SDP - Offer');
-    console.log(JSON.stringify(sdp));
-
-    peerConn.setLocalDescription(sdp).then((dec) => {
-      socket.emit('send_offer', sdp);
-    });
+function onLocalSessionCreated(desc) {
+  console.log('local session created:', desc);
+  peerConn.setLocalDescription(desc).then(() => {
+    console.log('sending local desc:', peerConn.localDescription);
+    sendMessage(peerConn.localDescription);
   });
-
-  peerConn.onicecandidate = (obj)=>{
-    //console.log('onicecandidate');
-    //console.log(JSON.stringify(obj.candidate));
-  };
 }
 
-function setDataChannel(){
-  peerConn.ondatachannel = function(event) {
-    console.log('ondatachannel:', event.channel);
-    dataChannel = event.channel;
+function onDataChannelCreated(channel) {
+  console.log('onDataChannelCreated:', channel);
+
+  channel.onopen = function() {
+    console.log('CHANNEL opened!!!');
+    sendBtn.disabled = false;
+    snapAndSendBtn.disabled = false;
   };
 
-  dataChannel.onmessage = function (event) {
-    console.log('########## RECEIVED SOME AWESOME DATA ##########');
-    console.log(event.data);
+  channel.onclose = function () {
+    console.log('Channel closed.');
+    sendBtn.disabled = true;
+    snapAndSendBtn.disabled = true;
+  }
+
+  channel.onmessage = (data) => {
+    console.log('Received some data');
+    console.log(data);
   }
 }
 
-function sendData() {
-  window.testData = prompt('Enter data:');
-  dataChannel.send(testData);
-}
+/****************************************************************************
+ * Signaling server
+ ****************************************************************************/
 
-// SIGNAL
-
+// Connect to the signaling server
+let socket = io.connect();
 let isInitiator;
-const socket = io.connect();
 
-function frontendPrint(str) {
-  document.getElementById('demo').innerHTML += '<br/>' + str;
-}
-
-function joinCreate(){
-  window.room = prompt('Enter room name:');
-  if (room !== '') {
-    console.log('Message from client: Asking to join room ' + room);
-    socket.emit('create or join', room);
-  }
-}
+socket.on('ipaddr', function(ipaddr) {
+  console.log('Server IP address is: ' + ipaddr);
+});
 
 socket.on('created', function(room, clientId) {
+  console.log('Created room', room, '- my client ID is', clientId);
   isInitiator = true;
 });
 
-socket.on('received_offer', function(msg) {
-  console.log('Client: offer received' );
-
-  peerConn.setRemoteDescription(msg).then(() => {
-    console.log('Client: remote description set' );
-
-    // Todo: setup video etc
-
-    peerConn.createAnswer().then((sdp) => {
-      peerConn.setLocalDescription(sdp).then((dec) => {
-        socket.emit('send_answer', sdp);
-      });
-    });
-  });
-});
-
-socket.on('received_answer', function(msg) {
-  console.log('Client: answer received');
-
-  peerConn.setRemoteDescription(msg).then(() => {
-    console.log('Session established');
-  });
-});
-
-socket.on('full', function(room) {
-  console.log('Message from client: Room ' + room + ' is full :^(');
-});
-
-socket.on('ipaddr', function(ipaddr) {
-  console.log('Message from client: Server IP address is ' + ipaddr);
-});
-
 socket.on('joined', function(room, clientId) {
+  console.log('This peer has joined room', room, 'with client ID', clientId);
   isInitiator = false;
+  createPeerConnection(isInitiator, STUN_SERVER);
+});
+
+// socket.on('full', function(room) {
+//   alert('Room ' + room + ' is full. We will create a new room for you.');
+//   window.location.hash = '';
+//   window.location.reload();
+// });
+
+socket.on('ready', function() {
+  console.log('Socket is ready');
+  createPeerConnection(isInitiator, STUN_SERVER);
 });
 
 socket.on('log', function(array) {
   console.log.apply(console, array);
 });
 
-socket.on('frontend', frontendPrint);
+socket.on('message', function(message) {
+  console.log('Client received message:', message);
+  signalingMessageCallback(message);
+});
+
+// if (location.hostname.match(/localhost|127\.0\.0/)) {
+//   socket.emit('ipaddr');
+// }
+
+// Leaving rooms and disconnecting from peers.
+// socket.on('disconnect', function(reason) {
+//   console.log(`Disconnected: ${reason}.`);
+//   sendBtn.disabled = true;
+//   snapAndSendBtn.disabled = true;
+// });
+//
+// socket.on('bye', function(room) {
+//   console.log(`Peer leaving room ${room}.`);
+//   sendBtn.disabled = true;
+//   snapAndSendBtn.disabled = true;
+//   // If peer did not create the room, re-enter to be creator.
+//   if (!isInitiator) {
+//     window.location.reload();
+//   }
+// });
+
+// window.addEventListener('unload', function() {
+//   console.log(`Unloading window. Notifying peers in ${room}.`);
+//   socket.emit('bye', room);
+// });
+
+
+/**
+ * Send message to signaling server
+ */
+function sendMessage(message) {
+  console.log('Client sending message: ', message);
+  socket.emit('message', message);
+}
+
+/****************************************************************************
+ * Interaction
+ ****************************************************************************/
+
+// Joining a room.
+window.channel = prompt('Room?');
+socket.emit('create or join', channel);
+
+function sendData(){
+  console.log('Datachannel:', dataChannel);
+}
--- demo/public-data-channel/signal-server/main.js
+++ demo/public-data-channel/signal-server/main.js
@@ -20,58 +20,44 @@ const io = socketIO.listen(app);
 
 io.sockets.on('connection', function(socket) {
 
+  // convenience function to log server messages on the client
   function log() {
     var array = ['Message from server:'];
     array.push.apply(array, arguments);
     socket.emit('log', array);
   }
 
-  socket.on('send_offer', function(message) {
+  socket.on('message', function(message) {
     log('Client said: ', message);
-    socket.broadcast.emit('received_offer', message);
-  });
-
-  socket.on('send_answer', function(message) {
-    log('Client said: ', message);
-    socket.broadcast.emit('received_answer', message);
+    // for a real app, would be room-only (not broadcast)
+    socket.broadcast.emit('message', message);
   });
 
   socket.on('create or join', function(room) {
-
-    socket.emit('frontend', 'Number of rooms: ' + Object.keys(io.sockets.adapter.rooms).length);
-    socket.emit('frontend', 'Room: ' + room);
-
     log('Received request to create or join room ' + room);
 
     let clientsInRoom = io.sockets.adapter.rooms[room];
-    let numClients = clientsInRoom ?
-        Object.keys(clientsInRoom.sockets).length :
-        0;
-
+    let numClients = clientsInRoom ? Object.keys(clientsInRoom.sockets).length : 0;
     log('Room ' + room + ' now has ' + numClients + ' client(s)');
 
     if (numClients === 0) {
       socket.join(room);
       log('Client ID ' + socket.id + ' created room ' + room);
       socket.emit('created', room, socket.id);
-
     } else if (numClients === 1) {
       log('Client ID ' + socket.id + ' joined room ' + room);
-      io.sockets.in(room).emit('join', room);
       socket.join(room);
       socket.emit('joined', room, socket.id);
-      io.sockets.in(room).emit('ready');
+      io.sockets.in(room).emit('ready', room);
+      socket.broadcast.emit('ready', room);
     } else { // max two clients
       socket.emit('full', room);
     }
   });
 
   socket.on('ipaddr', function() {
-    const ifaces = os.networkInterfaces();
-
-    console.log('ipaddr - DEBUG');
-
-    for (let dev in ifaces) {
+    var ifaces = os.networkInterfaces();
+    for (var dev in ifaces) {
       ifaces[dev].forEach(function(details) {
         if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
           socket.emit('ipaddr', details.address);
@@ -80,6 +66,12 @@ io.sockets.on('connection', function(socket) {
     }
   });
 
+  // socket.on('disconnect', function(reason) {
+  //   console.log(`Peer or server disconnected. Reason: ${reason}.`);
+  //   socket.broadcast.emit('bye');
+  // });
+  //
+  // socket.on('bye', function(room) {
+  //   console.log(`Peer said bye on room ${room}.`);
+  // });
 });
-
-module.exports = app;
