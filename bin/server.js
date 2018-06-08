7bc99bb87aba9c3bb62c887a465914a41f24f0c8
Maximilian Kroschewski <jmakro@posteo.de>
2018-06-08 13:24:08 +0200

    babel support

--- bin/server.js
+++ bin/server.js
@@ -1,17 +1,15 @@
-'use strict';
-
 const os = require('os');
 const fs = require('fs');
 const nodeStatic = require('node-static');
-const SignalServer = require('../src/signaling');
+const SignalServer = require('./src/signaling');
 const https = require('https');
 
 const port = 8080;
 const clientPath = '../web_app';
 
 const options = {
-  key: fs.readFileSync('../assets/cert/signal.key'),
-  cert: fs.readFileSync('../assets/cert/signal.crt')
+  key: fs.readFileSync('./assets/cert/signal.key'),
+  cert: fs.readFileSync('./assets/cert/signal.crt')
 };
 
 const fileServer = new nodeStatic.Server(clientPath);
