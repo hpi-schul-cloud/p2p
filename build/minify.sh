8e430897df07a2ea05b91a021cbf29fdeba91d17
Maximilian Kroschewski <jmakro@posteo.de>
2018-07-07 13:03:18 +0200

    less verbose server logs, remove minify compressor

--- build/minify.sh
+++ build/minify.sh
@@ -1,3 +1,4 @@
-node-minify --compressor babel-minify --input ./build/tmp/middleware.js,./build/tmp/signaling.js,./build/tmp/peer.js,./build/tmp/p2pCDN.js --output ./web_app/js/p2pCDN.js
 
-node-minify --compressor babel-minify --input ./build/tmp/utils.js --output ./web_app/js/utils.js
+node-minify --compressor no-compress --input ./build/tmp/middleware.js,./build/tmp/signaling.js,./build/tmp/peer.js,./build/tmp/p2pCDN.js --output ./web_app/js/p2pCDN.js
+
+node-minify --compressor no-compress --input ./build/tmp/utils.js --output ./web_app/js/utils.js
