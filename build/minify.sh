458604d84b2c67134e3884c56565b7ca16cfd387
t.friedrich <t.friedrich@mediaevent.services>
2019-10-31 14:40:21 +0100

    Adds webpacker to do a final bundling of the file to ensure scopes

--- build/minify.sh
+++ build/minify.sh
@@ -1,4 +1,6 @@
 node-minify --compressor no-compress --input ./build/lib/idb-keyval.js,./build/lib/debug.js,./build/tmp/middleware.js,./build/tmp/signaling.js,./build/tmp/peer.js,./build/tmp/systemTest.js,./build/tmp/p2pCDN.js,./build/tmp/FayeConnection.js,./build/lib/modernizr.js,./build/tmp/utils.js --output ./build/src/p2pCDN.js
+npx webpack
 node-minify --compressor no-compress --input ./build/lib/idb-keyval.js,./src/client/js/p2pCDNsw.js,./build/tmp/utils.js,./src/client/js/swPeerNotifications.js --output ./build/src/p2pCDNsw.js
+#browserify ./build/src/p2pCDN.js --standalone P2pCDN > ./build/src/p2pCDN.js
 cp ./build/src/p2pCDN.js ./vendor/assets/javascript/p2pCDN.js
 cp ./build/src/p2pCDNsw.js ./public/p2pCDNsw.js
